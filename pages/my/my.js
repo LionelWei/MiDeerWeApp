7de8866182bbfaeac630bf15192cb05aa720051f
LionelWei <lionel.wei@outlook.com>
2017-02-15 22:12:23 +0800

    add http request test

--- pages/my/my.js
+++ pages/my/my.js
@@ -10,5 +10,26 @@ Page({
     this.setData({
       courseTitle:"小小米鹿班"
     })
+    let that = this;
+    wx.request({
+      url: 'http://open.play.cn/api/v2/egame/host.json/',
+      data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function(res){
+        // success
+        setTimeout(() => {
+        that.setData({
+          courseTitle: res.data
+        })
+        }, 1000)
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
   },
 })
