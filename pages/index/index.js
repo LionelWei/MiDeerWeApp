b4cd765590813da7e2a0aea4a875a25e21614cb7
LionelWei <lionel.wei@outlook.com>
2017-02-17 01:37:46 +0800

    add map and contact-button; remove tabbar

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,27 +9,34 @@ var courses = [{ desc: "小小米鹿班", age: "3-4岁", number: "8人/班" }
   , { desc: "神奇探索班", age: '9-10岁', number: "10人/班" }
   , { desc: "大师创作班", age: '11-12岁', number: "10人/班" }
   , { desc: "创意素描水粉班", age: '13-14岁', number: "10人/班" }];
-var imgUrls = [
-  'https://p1.meituan.net/dpdeal/2d2450529fd38d5363fb3882528e151a76430.jpg',
-  'https://p1.meituan.net/dpdeal/0adc683a3efc6e6a54de618b66014fe12593493.jpg',
-  'https://p1.meituan.net/dpdeal/6fb12fde5d0cea480bdf5939bc0c0c114306877.jpg',
-  'https://p0.meituan.net/dpdeal/c8dc99b9fee2e0ae0941a4d1896c9ae43945579.jpg',
-  'https://p0.meituan.net/dpdeal/d5c25ca68dcc059addded451e069400960133.jpg'
-];
+// var imgUrls = [
+//   'https://p1.meituan.net/dpdeal/2d2450529fd38d5363fb3882528e151a76430.jpg',
+//   'https://p1.meituan.net/dpdeal/0adc683a3efc6e6a54de618b66014fe12593493.jpg',
+//   'https://p1.meituan.net/dpdeal/6fb12fde5d0cea480bdf5939bc0c0c114306877.jpg',
+//   'https://p0.meituan.net/dpdeal/c8dc99b9fee2e0ae0941a4d1896c9ae43945579.jpg',
+//   'https://p0.meituan.net/dpdeal/d5c25ca68dcc059addded451e069400960133.jpg'
+// ];
 
-var imagesWithId = imgUrls.map((e, i) => {
-  return {
-    url: e,
-    id: i
-  }
-});
+// var imagesWithId = imgUrls.map((e, i) => {
+//   return {
+//     url: e,
+//     id: i
+//   }
+// });
 
 Page({
   isClicked: true,
   data: {
-    bannerImages: imagesWithId,
-    courses: courses,
-    indicatorDots: true
+    bannerImages: null,
+    courses: null,
+    markers: [{
+      iconPath: "/image/map_marker.png",
+      id: 0,
+      latitude: 118.793990,
+      longitude: 32.040110,
+      width: 64,
+      height: 64
+    }],
   },
   //事件处理函数
   bindViewTap: function () {
@@ -48,7 +55,7 @@ Page({
       })
       console.log(JSON.stringify(userInfo, null, 2));
     })
-    this.getData();
+    this.updateData();
   },
   onCourseTap: function () {
     // wx.showModal({title: "u r clicked"})
@@ -84,29 +91,55 @@ Page({
     var id = e.currentTarget.id, list = this.data.bannerImages;
     console.log(id + ' is clicked: ' + list[id].url);
   },
-  getData: function () {
-    var Diary = bmob.Object.extend("mideer1");
-    var query = new bmob.Query(Diary);
+  updateData: function () {
+    this.getBanner();
+    this.getCourses();
+  },
+  getBanner: function() {
+    var query = new bmob.Query(bmob.Object.extend("res"));
+    var that = this;
     // 查询所有数据
+    query.equalTo('showAt', 'banner');
     query.find({
       success: function (results) {
-        // wx.showModal({
-        //   title: "成功",
-        //   content: "共查询到 " + results.length + " 条记录"
-        // })
-        // 循环处理查询到的数据
-        for (var i = 0; i < results.length; i++) {
-          var object = results[i];
-          console.log(object.id + ' - ' + object.get('title'));
-        }
+        var images = results.map((e, i) => {
+          return {
+            url: e.get('url')._url,
+            id: i,
+          }
+        })
+        that.setData({
+          bannerImages: images
+        })
       },
       error: function (error) {
-        // wx.showModal({
-        //   title: "失败",
-        //   content: "查询失败: " + error.code + " " + error.message
-        // })
         console.log("查询失败: " + error.code + " " + error.message);
       }
     });
+  },
+  getCourses: function() {
+    var query = new bmob.Query(bmob.Object.extend("courses"));
+    var that = this;
+    // 查询所有数据
+    query.find({
+      success: function (results) {
+        var courses = results.map((e, i) => {
+          return {
+            courseType: e.get('courseType'),
+            courseNum: e.get('courseNum'),
+            courseName: e.get('courseName'),
+            ageRange: e.get('ageRange'),
+            icon: e.get('courseFirstImage')._url,
+            feature: e.get('feature'),
+          }
+        })
+        that.setData({
+          courses: courses
+        })
+      },
+      error: function (error) {
+        console.log("查询失败: " + error.code + " " + error.message);
+      }
+    });    
   }
 })
